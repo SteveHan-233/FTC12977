de6fe08ed40d7086f1c735fb71eed5a5288e8921
nihal mitta <nihal.mitta@gmail.com>
2018-12-10 17:36:31 -0600

    lol

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/goldDetectionAuto.java
@@ -0,0 +1,34 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+
+import java.util.List;
+import java.util.Locale;
+
+@Autonomous(name="Gold Detection")
+//@Disabled
+public class goldDetectionAuto extends LinearOpMode
+{
+    // called when init button is  pressed.
+    @Override
+    public void runOpMode() throws InterruptedException
+    {
+    
+    }
+
+}
